22f3800c557b8ba688fe88dfd48c329fae3277c0
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-10 18:14:34 +0300

    GView auto formating code

--- ui/GView.cpp
+++ ui/GView.cpp
@@ -1,114 +1,93 @@
 #include "GView.hpp"
 
+#include <QDebug>
 #include <QFileDialog>
 
-#include "tab/Tab.hpp"
-#include "tab/tools/GtpFiles.hpp"
-#include "midi/MidiFile.hpp"
-
 #include "g0/Config.hpp"
-
-#include "tab/tools/GmyFile.hpp"
-#include "tab/tools/TabLoader.hpp" //loader
 #include "g0/Threads.hpp"
+#include "midi/MidiFile.hpp"
+#include "tab/Tab.hpp"
+#include "tab/tools/GmyFile.hpp"
+#include "tab/tools/GtpFiles.hpp"
+#include "tab/tools/TabLoader.hpp"  //loader
 
-#include <QDebug>
-
-
-bool gViewLog=false;
-
+bool gViewLog = false;
 
 using namespace gtmy;
 
+GView *MasterView::changeChild(GView *newChild) {
+  GView *oldCh = child;
 
-GView *MasterView::changeChild(GView *newChild)
-{      
-      GView *oldCh = child;
+  lastView = oldCh;
 
-      lastView = oldCh;
+  child = newChild;
+  if (child) child->setMaster(this);
+  if (oldCh)
+    oldCh->setMaster(0);
+  else
+    firstChld = newChild;
 
-      child = newChild;
-      if (child)
-      child->setMaster(this);
-      if (oldCh)
-          oldCh->setMaster(0);
-      else
-          firstChld = newChild;
+  // must repaint
 
-      //must repaint
-
-      return oldCh;
-
+  return oldCh;
 }
 
+GLabel::GLabel(int x, int y, std::string text, std::string pressSyn,
+               bool showBord)
+    : visible(true), showBorder(showBord) {
+  int size = text.length();
+  // int height = 12;
+  this->x = x;
+  this->y = y;
+  h = 20;
+  w = size * 6 + 20;
+  ownText = text;
+  pressSynonim = pressSyn;
 
-GLabel::GLabel(int x, int y, std::string text, std::string pressSyn, bool showBord):
-    visible(true), showBorder(showBord)
-{
-    int size = text.length();
-    //int height = 12;
-    this->x = x;
-    this->y = y;
-    h = 20;
-    w = size*6 + 20;
-    ownText = text;
-    pressSynonim = pressSyn;
+  if (CONF_PARAM("images") == "1") {
+    // later preloaded images should apear
+    // REFACT: use map
 
-    if (CONF_PARAM("images")=="1")
-    {
-        //later preloaded images should apear
-        //REFACT: use map
+    QImage *imgPtr = ImagePreloader::getInstance().getImage(text);
 
-       QImage* imgPtr = ImagePreloader::getInstance().getImage(text);
+    /*
+    if ((text =="prevBeat") || (text =="nextBeat") || (text =="upString") ||
+    (text =="downString") || (text =="prevBar") || (text =="nextBar") || (text
+    =="play") || (text =="save") || (text=="open")|| (text =="new")
+            || (text =="config")
+            || (text =="record")|| (text =="tap")
+            || (text =="pattern")|| (text =="tests")||
+            (text=="tab")||(text=="backview")||
+            (text=="morze")||(text=="info")||(text=="openPannel")||
+            (text=="1")||(text=="2")||(text=="3")||
+            (text=="4")||(text=="5")||(text=="6")||
+            (text=="7")||(text=="8")||(text=="9")||
+            (text=="0")||(text=="qp")||(text=="qm")||
+            (text=="p")||(text=="del")||(text==".")
+            ||(text=="-3-")||(text=="leeg")||(text=="x")) */
 
-        /*
-        if ((text =="prevBeat") || (text =="nextBeat") || (text =="upString") || (text =="downString") ||
-                (text =="prevBar") || (text =="nextBar") || (text =="play") || (text =="save") ||
-                (text=="open")|| (text =="new")
-                || (text =="config")
-                || (text =="record")|| (text =="tap")
-                || (text =="pattern")|| (text =="tests")||
-                (text=="tab")||(text=="backview")||
-                (text=="morze")||(text=="info")||(text=="openPannel")||
-                (text=="1")||(text=="2")||(text=="3")||
-                (text=="4")||(text=="5")||(text=="6")||
-                (text=="7")||(text=="8")||(text=="9")||
-                (text=="0")||(text=="qp")||(text=="qm")||
-                (text=="p")||(text=="del")||(text==".")
-                ||(text=="-3-")||(text=="leeg")||(text=="x")) */
-
-        if (imgPtr)
-        {
-            imageLabel = std::make_unique<GImage>(x,y,text);
-            setW(imageLabel->getW());
-            setH(imageLabel->getH());
-        }
+    if (imgPtr) {
+      imageLabel = std::make_unique<GImage>(x, y, text);
+      setW(imageLabel->getW());
+      setH(imageLabel->getH());
     }
-
+  }
 }
 
-bool GLabel::hit(int hX, int hY)
-{
-    bool hitten = false;
+bool GLabel::hit(int hX, int hY) {
+  bool hitten = false;
 
-    if (imageLabel==0)
-    {
-        y = y - h/2;
-         hitten = GView::hit(hX,hY);
-        y = y + h/2;
-    }
-    else
-    {
-        hitten = GView::hit(hX,hY);
-    }
+  if (imageLabel == 0) {
+    y = y - h / 2;
+    hitten = GView::hit(hX, hY);
+    y = y + h / 2;
+  } else {
+    hitten = GView::hit(hX, hY);
+  }
 
-    if (hitten)
-    {
-        qDebug()<<"GLabel hitten "<<this->getText().c_str();
-    }
+  if (hitten) {
+    qDebug() << "GLabel hitten " << this->getText().c_str();
+  }
 
-    return hitten;
+  return hitten;
 }
-
-
-
--- ui/GView.hpp
+++ ui/GView.hpp
@@ -1,291 +1,277 @@
 #ifndef GVIEW_H
 #define GVIEW_H
 
+#include <time.h>
 
-#include "tab/Tab.hpp"
-#include "g0/Config.hpp"
-#include "ui/ImagePreloader.hpp"
-
-#include "tab/tools/Commands.hpp"
-
+#include <QDebug>
 #include <QPainter>
 
-#include <time.h>
-#include <QDebug>
-
+#include "g0/Config.hpp"
+#include "tab/Tab.hpp"
+#include "tab/tools/Commands.hpp"
+#include "ui/ImagePreloader.hpp"
 
 namespace gtmy {
 
 
-    class MasterView;
 
-    class GView
-    {
-    protected:
-    int x;
-    int y;
-    int w;
-    int h;
-
-    MasterView *master;
+class MasterView;
 
 
-
-    public:
-        GView():x(0),y(0),w(0),h(0),master(0){}
-        GView(int xV,int yV, int wV, int hV):x(xV),y(yV),w(wV),h(hV),master(0){}
-        virtual ~GView() {}
-
-        virtual void setMaster(MasterView *mast) { master = mast; }
-        MasterView *getMaster() { return master; }
-
-        virtual bool isMovableX() { return false; }
-        virtual bool isMovableY() { return false; }
 
-        virtual void setUI() { }
+class GView {
+ protected:
+  int x;
+  int y;
+  int w;
+  int h;
 
-        int getX(){return x;}
-        int getY() { return y; }
-        int getW() {return w; }
-        int getH() {return h; }
+  MasterView *master;
 
-        void setW(int newW) { w = newW; }
-        void setH(int newH) { h = newH; }
-        void setX(int newX) { x = newX; }
-        void setY(int newY) { y = newY; }
+ public:
+  GView() : x(0), y(0), w(0), h(0), master(0) {}
+  GView(int xV, int yV, int wV, int hV)
+      : x(xV), y(yV), w(wV), h(hV), master(0) {}
+  virtual ~GView() {}
 
-        virtual void draw([[maybe_unused]]QPainter *painter){ } //virt
+  virtual void setMaster(MasterView *mast) { master = mast; }
+  MasterView *getMaster() { return master; }
 
-        virtual bool hit(int hX, int hY)
-        {
-            if ((hX >= x)
-               && (hY >=y))
-            {
-                int xDiff = hX-x;
-                int yDiff = hY-y;
+  virtual bool isMovableX() { return false; }
+  virtual bool isMovableY() { return false; }
 
-                if ((xDiff<=w)&&
-                    (yDiff<=h))
-                    return true;
-            }
-            return false;
-        }
+  virtual void setUI() {}
 
+  int getX() { return x; }
+  int getY() { return y; }
+  int getW() { return w; }
+  int getH() { return h; }
 
-    public:
-        virtual void onclick([[maybe_unused]] int x1, [[maybe_unused]] int y1){ } //virt
-        virtual void ondblclick([[maybe_unused]] int x1, [[maybe_unused]] int y1){} //virt
-        virtual void keyevent([[maybe_unused]] std::string press){} //virt
-        virtual void ongesture([[maybe_unused]] int offset, [[maybe_unused]] bool horizontal) {}
+  void setW(int newW) { w = newW; }
+  void setH(int newH) { h = newH; }
+  void setX(int newX) { x = newX; }
+  void setY(int newY) { y = newY; }
 
-        virtual void onTabCommand([[maybe_unused]]TabCommand command) {
-        }
+  virtual void draw([[maybe_unused]] QPainter *painter) {}  // virt
 
-        virtual void onTrackCommand([[maybe_unused]]TrackCommand command) {
-            qDebug() << "EMPY on TrackCommand";
-        }
+  virtual bool hit(int hX, int hY) {
+    if ((hX >= x) && (hY >= y)) {
+      int xDiff = hX - x;
+      int yDiff = hY - y;
 
-    };
+      if ((xDiff <= w) && (yDiff <= h)) return true;
+    }
+    return false;
+  }
 
-    class GImage : public GView
-    {
-    private:
-        std::string imageName;
+ public:
+  virtual void onclick([[maybe_unused]] int x1, [[maybe_unused]] int y1) {
+  }  // virt
+  virtual void ondblclick([[maybe_unused]] int x1, [[maybe_unused]] int y1) {
+  }                                                             // virt
+  virtual void keyevent([[maybe_unused]] std::string press) {}  // virt
+  virtual void ongesture([[maybe_unused]] int offset,
+                         [[maybe_unused]] bool horizontal) {}
 
-    public:
-        GImage(int x, int y, std::string imgName):GView(x,y,36,36),imageName(imgName)
-        {
-        }
+  virtual void onTabCommand([[maybe_unused]] TabCommand command) {}
 
-        void draw(QPainter *painter)
-        {
-           QImage *img = (QImage*) ImagePreloader::getInstance().getImage(imageName);
-           if (img)
-               painter->drawImage(x,y,*img);
-        }
+  virtual void onTrackCommand([[maybe_unused]] TrackCommand command) {
+    qDebug() << "EMPY on TrackCommand";
+  }
+};
 
-    };
 
-    class GRect : public GView
-    {
-        int colorPress;
 
-    public:
-        GRect(int x, int y, int w, int h):GView(x,y,w,h),colorPress(0)
-        {
+class GImage : public GView {
+ private:
+  std::string imageName;
 
-        }
+ public:
+  GImage(int x, int y, std::string imgName)
+      : GView(x, y, 36, 36), imageName(imgName) {}
 
-        void pressIt()
-        {
-            ++colorPress;
-            if (colorPress>10)
-                colorPress = 0;
-        }
+  void draw(QPainter *painter) {
+    QImage *img = (QImage *)ImagePreloader::getInstance().getImage(imageName);
+    if (img) painter->drawImage(x, y, *img);
+  }
+};
 
-        void draw(QPainter *painter)
-        {
-            painter->drawRect(x,y,w,h);
 
-            painter->fillRect(x+10,y+10,w-20,h-20,colorPress);
-        }
-    };
 
-    class GLabel : public GView
-    {
-    private:
-        std::string ownText;
-        std::string pressSynonim;
+class GRect : public GView {
+  int colorPress;
 
-        std::unique_ptr<GImage> imageLabel;
+ public:
+  GRect(int x, int y, int w, int h) : GView(x, y, w, h), colorPress(0) {}
 
-        bool visible;
+  void pressIt() {
+    ++colorPress;
+    if (colorPress > 10) colorPress = 0;
+  }
 
-        bool showBorder;
+  void draw(QPainter *painter) {
+    painter->drawRect(x, y, w, h);
 
-        public:
+    painter->fillRect(x + 10, y + 10, w - 20, h - 20, colorPress);
+  }
+};
 
-            bool isVisible() { return visible; }
-            void setVisible(bool value) {visible     = value; }
 
-            void setBorder(bool nowShowBorder) { showBorder = nowShowBorder; }
 
-            void setText(std::string newText) { ownText = newText; }
-            std::string getText() { return ownText; }
+class GLabel : public GView {
+ private:
+  std::string ownText;
+  std::string pressSynonim;
 
-            GLabel(int x, int y, std::string text, std::string pressSyn="", bool showBord=true);
+  std::unique_ptr<GImage> imageLabel;
 
-            std::string getPressSyn(){return pressSynonim;}
+  bool visible;
 
-            void draw(QPainter *painter){
+  bool showBorder;
 
-                if (visible==false)
-                    return;
+ public:
+  bool isVisible() { return visible; }
+  void setVisible(bool value) { visible = value; }
 
-                if (imageLabel==0)
-                {
-                    painter->drawText(x+5,y,ownText.c_str());
+  void setBorder(bool nowShowBorder) { showBorder = nowShowBorder; }
 
-                    if (showBorder)
-                        painter->drawRect(x,y-15,w,h); //10 half of text height
-                }
-                else
-                {
-                    imageLabel->setX(x); imageLabel->setY(y-10);
-                    imageLabel->draw(painter);
+  void setText(std::string newText) { ownText = newText; }
+  std::string getText() { return ownText; }
 
-                    if (showBorder)
-                        painter->drawRect(x,y-10,imageLabel->getW(),imageLabel->getH());
+  GLabel(int x, int y, std::string text, std::string pressSyn = "",
+         bool showBord = true);
 
-                }
-            }
+  std::string getPressSyn() { return pressSynonim; }
 
-            virtual bool hit(int hX, int hY);
+  void draw(QPainter *painter) {
+    if (visible == false) return;
 
-    };
+    if (imageLabel == 0) {
+      painter->drawText(x + 5, y, ownText.c_str());
 
+      if (showBorder)
+        painter->drawRect(x, y - 15, w, h);  // 10 half of text height
+    } else {
+      imageLabel->setX(x);
+      imageLabel->setY(y - 10);
+      imageLabel->draw(painter);
 
+      if (showBorder)
+        painter->drawRect(x, y - 10, imageLabel->getW(), imageLabel->getH());
+    }
+  }
 
-    class ThreadLocal;
+  virtual bool hit(int hX, int hY);
+};
 
-    class MasterView
-    {
-    private:
-        GView *child;
 
-        GView *firstChld;
 
-        GView *lastView;
+class ThreadLocal;
 
-    public:
-          MasterView():child(0){}
-          MasterView(GView *newChild):child(newChild),firstChld(newChild){ child->setMaster(this); lastView=child;}
+class MasterView {
+ private:
+  GView *child;
 
+  GView *firstChld;
 
-          GView *getChild() { return child; }
-          GView *changeChild(GView *newChild);
-          GView *resetToFirstChild(){ return changeChild(firstChld); }
-          GView *resetToLastView() { return changeChild(lastView); }
+  GView *lastView;
 
-          GView *getFirstChild() { return firstChld; }
+ public:
+  MasterView() : child(0) {}
+  MasterView(GView *newChild) : child(newChild), firstChld(newChild) {
+    child->setMaster(this);
+    lastView = child;
+  }
 
-          virtual void connectThread([[maybe_unused]]std::unique_ptr<ThreadLocal>& thr){}
-          virtual void connectMainThread([[maybe_unused]]std::unique_ptr<ThreadLocal>& thr){}
+  GView *getChild() { return child; }
+  GView *changeChild(GView *newChild);
+  GView *resetToFirstChild() { return changeChild(firstChld); }
+  GView *resetToLastView() { return changeChild(lastView); }
 
-          virtual void pleaseRepaint() {}
+  GView *getFirstChild() { return firstChld; }
 
-          virtual int getWidth() { return 0; }
-          virtual int getHeight() { return 0; }
+  virtual void connectThread([
+      [maybe_unused]] std::unique_ptr<ThreadLocal> &thr) {}
+  virtual void connectMainThread([
+      [maybe_unused]] std::unique_ptr<ThreadLocal> &thr) {}
 
-          virtual void requestHeight([[maybe_unused]]int newH) {}
-          virtual void requestWidth([[maybe_unused]]int newW) {}
+  virtual void pleaseRepaint() {}
 
-          virtual int getToolBarHeight() { return 0;}
-          virtual int getStatusBarHeight() { return 0;}
+  virtual int getWidth() { return 0; }
+  virtual int getHeight() { return 0; }
 
-          virtual void SetButton([[maybe_unused]]int index, [[maybe_unused]]std::string text, [[maybe_unused]]int x1, [[maybe_unused]]int y1, [[maybe_unused]]int w1, [[maybe_unused]]int h1, [[maybe_unused]]std::string pressSyn)
-          {}
+  virtual void requestHeight([[maybe_unused]] int newH) {}
+  virtual void requestWidth([[maybe_unused]] int newW) {}
 
-          virtual void SetButton(int index, GLabel *w, std::string pressSyn) {
-              SetButton(index,w->getText(),w->getX(),w->getY(),w->getW(),w->getH(),pressSyn);
-          }
+  virtual int getToolBarHeight() { return 0; }
+  virtual int getStatusBarHeight() { return 0; }
 
-          virtual void setComboBox([[maybe_unused]]int index, [[maybe_unused]]std::string, [[maybe_unused]]int x1, [[maybe_unused]]int y1, [[maybe_unused]]int w1, [[maybe_unused]]int h1, [[maybe_unused]]int forceValue)
-          {}
+  virtual void SetButton([[maybe_unused]] int index,
+                         [[maybe_unused]] std::string text,
+                         [[maybe_unused]] int x1, [[maybe_unused]] int y1,
+                         [[maybe_unused]] int w1, [[maybe_unused]] int h1,
+                         [[maybe_unused]] std::string pressSyn) {}
 
-          virtual void renewComboParams([[maybe_unused]]int index, [[maybe_unused]]std::string params)
-          {
+  virtual void SetButton(int index, GLabel *w, std::string pressSyn) {
+    SetButton(index, w->getText(), w->getX(), w->getY(), w->getW(), w->getH(),
+              pressSyn);
+  }
 
-          }
+  virtual void setComboBox([[maybe_unused]] int index,
+                           [[maybe_unused]] std::string,
+                           [[maybe_unused]] int x1, [[maybe_unused]] int y1,
+                           [[maybe_unused]] int w1, [[maybe_unused]] int h1,
+                           [[maybe_unused]] int forceValue) {}
 
-          virtual void setViewPannel([[maybe_unused]]int val) {}
-          virtual int getComboBoxValue([[maybe_unused]]int index) { return -1; }
-          virtual void showHelp() {}
-          virtual void setStatusBarMessage([[maybe_unused]]int index, [[maybe_unused]]std::string text, [[maybe_unused]]int timeOut=0) {}
-          virtual void pushForceKey([[maybe_unused]]std::string keyevent) {}
-          virtual bool isPlaying() { return false; }
-          virtual void addToPlaylist(std::vector<std::string> playElement) { Q_UNUSED(playElement); }
-          virtual bool isPlaylistHere() { return false; }
-          virtual void goOnPlaylist() {}
-          virtual void cleanPlayList() {}
+  virtual void renewComboParams([[maybe_unused]] int index,
+                                [[maybe_unused]] std::string params) {}
 
-    };
+  virtual void setViewPannel([[maybe_unused]] int val) {}
+  virtual int getComboBoxValue([[maybe_unused]] int index) { return -1; }
+  virtual void showHelp() {}
+  virtual void setStatusBarMessage([[maybe_unused]] int index,
+                                   [[maybe_unused]] std::string text,
+                                   [[maybe_unused]] int timeOut = 0) {}
+  virtual void pushForceKey([[maybe_unused]] std::string keyevent) {}
+  virtual bool isPlaying() { return false; }
+  virtual void addToPlaylist(std::vector<std::string> playElement) {
+    Q_UNUSED(playElement);
+  }
+  virtual bool isPlaylistHere() { return false; }
+  virtual void goOnPlaylist() {}
+  virtual void cleanPlayList() {}
+};
 
 
 
-    class GCheckButton : public GView
-    {
-    private:
+class GCheckButton : public GView {
+ private:
+  bool checked;
 
-        bool checked;
+ public:
+  GCheckButton(int x1, int y1, int w1, int h1)
+      : GView(x1, y1, w1, h1), checked(false) {}
 
-    public:
+  bool isChecked() { return checked; }
 
-        GCheckButton(int x1, int y1, int w1, int h1):GView(x1,y1,w1,h1),
-            checked(false)
-        {}
+  void draw(QPainter *painter) {
+    if (checked)
+      painter->fillRect(x, y, w, h, Qt::darkGray);  // painter->fillRect(x,y,w,h,CONF_PARAM("colors.curBar"));
+                                                    // //5 some color TODO?
 
-        bool isChecked() { return checked; }
+    painter->drawRect(x, y, w, h);
+  }
 
-        void draw(QPainter *painter)
-        {
-            if (checked)
-                painter->fillRect(x,y,w,h,Qt::darkGray);//painter->fillRect(x,y,w,h,CONF_PARAM("colors.curBar")); //5 some color TODO?
+  virtual void onclick([[maybe_unused]] int x1, [[maybe_unused]] int y1) {
+    if (checked)
+      checked = false;
+    else
+      checked = true;
+  }
 
-            painter->drawRect(x,y,w,h);
-        }
+  virtual void ondblclick(int x1, int y1) { onclick(x1, y1); }
+};
 
-        virtual void onclick([[maybe_unused]]int x1, [[maybe_unused]]int y1)
-        {
-            if (checked)
-                checked = false;
-            else
-                checked = true;
-        }
+}  // namespace gtmy
 
-        virtual void ondblclick(int x1, int y1) {onclick(x1,y1); }
-    };
-
-}
-
-#endif // GVIEW_H
+#endif  // GVIEW_H
