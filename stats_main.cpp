d5a9fdce9ef1b71c3779e11e516566d6c7ff3123
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-24 21:01:22 +0300

    Just TODO

--- stats_main.cpp
+++ stats_main.cpp
@@ -6,9 +6,15 @@ using namespace gtmy;
 
 int main(int argc, char *argv[])
 {
+    //TODO if argc == 1 search for base catalog close to executable
+    //if argc == 2 its path to catalog
+    //if argc == 3 its path to csv (default is csv dir close)
+    //if argc == 4 then last is mode 'm' - generate midi files
+
+    //TODO handlers
 
     BaseStatistics base;
-    base.start("/home/punnalyse/dev/gtab/base/", 166000);
+    base.start("/home/punnalyse/dev/gtab/base/mine/", 207);
 
     QCoreApplication a(argc, argv);
     return a.exec();
